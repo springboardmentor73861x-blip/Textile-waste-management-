b1b38f92b8c0ccf76537726e7da7ce154cfee528
Brajnandan Prasad <brajnandanprasad21@gmail.com>
2026-08-01 21:04:02 +0530

    Add upload service layer

---
+++ backend/app/services/upload_service.py
@@ -0,0 +1,17 @@
+from fastapi import UploadFile
+
+from app.utils.file_handler import FileHandler
+
+
+class UploadService:
+
+    @staticmethod
+    async def upload_image(file: UploadFile):
+
+        image = await FileHandler.save_image(file)
+
+        return {
+            "message": "Image uploaded successfully.",
+            "filename": image["filename"],
+            "filepath": image["filepath"],
+        }
