2dd4c43d3dc6bec0c3ccc99e1dd02085b5b4c799
Shashipreethi <vshashipreethi@gmail.com>
2026-09-03 22:35:46 +0530

    Delete frontend directory

--- frontend/src/pages/Analytics.jsx
+++
@@ -1,9 +0,0 @@
-function Analytics() {
-    return (
-        <div>
-            <h1>Analytics Page</h1>
-        </div>
-    );
-}
-
-export default Analytics;
--- frontend/src/pages/DashboardCharts.jsx
+++
@@ -1,2 +0,0 @@
-// Kept as a compatibility re-export. The actual chart component lives in components.
-export { default } from "../components/DashboardCharts";
