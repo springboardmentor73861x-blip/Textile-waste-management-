41c7d05e671b0d9e0ab035afcdc9e4a02122d73f
Shashipreethi <vshashipreethi@gmail.com>
2026-09-03 22:35:18 +0530

    Delete backend directory

--- backend/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models.user import User
-from app.models.waste import WasteInventory
-from app.models.prediction import PredictionHistory
-from app.models.sustainable_fashion import SustainableFashion
